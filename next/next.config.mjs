a0a48c61c3c378bbd7d3ad977a1d2d5d2f022639
yuma0306 <yuma0306895@gmail.com>
2024-08-04 11:52:27 +0900

    fix config

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -1,6 +1,3 @@
-// const isProd = process.env.NODE_ENV === 'production'
-// const prefixPath = isProd ? '/docs' : ''
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -9,9 +6,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // ビルド時のデプロイ先のパスを指定
-  // basePath: prefixPath,
-  // assetPrefix: prefixPath,
 };
 
 export default nextConfig;
